a7338013b17b01705fc0eaffc3b88d2f05ee8f87
Audrey Sperry <audrey.sperry@gmail.com>
2017-08-16 20:43:58 -0400

    added javascript comments

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 
-
+//create pre set tasks
 const todos = [
   {
   "task": "wash car",
@@ -19,38 +19,31 @@ const todos = [
   "done": true
 }
 ];
-
-
-// find that task (return the index)
-// todos[index].done = !todos[index].done;
-
-
+//boiler plates
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//render the landing page with the pre-set tasks
 app.get('/', (req, res) => {
   res.render('index', {todos: todos});
 });
 
+//add tasks to todo list array
 app.post('/', (req, res) => {
   todos.push({task: req.body.todo, done: false});
-
-  console.log(todos);
 
   res.redirect('/');
 });
 
+//mark tasks complete and move to completed task section
 app.post('/mark', (req, res) => {
   let task = req.body.task;
-
-
   todos.forEach((todo)=>{
     todo.task === task ? (todo.done = !todo.done) : undefined
   });
-
 
   res.redirect('/');
 
